afe1df6ca283b603d8bddb4bdddd2fdefd8b8d3b
ehsan <ehsan2003.2003.382@gmail.com>
2023-09-08 14:01:12 +0330

    makes tokio runtime for kit single threaded so it consumes less resources

--- bin/praytimes-kit/src/main.rs
+++ bin/praytimes-kit/src/main.rs
@@ -1,8 +1,7 @@
 use std::error::Error;
 
 use clap::Parser;
-
-#[tokio::main]
+#[tokio::main(flavor = "current_thread")]
 async fn main() -> Result<(), Box<dyn Error>> {
     let args = praytimes_kit::Args::parse();
     praytimes_kit::run(args).await;
